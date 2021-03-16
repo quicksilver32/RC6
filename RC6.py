7412636ddfc1c04886ba3b8a26311d183e7f030a
quicksilver32 <ameliya9@mail.ru>
2021-03-16 03:17:18 +0300

    without bitarray

--- RC6.py
+++ RC6.py
@@ -1,27 +1,18 @@
 from utils import *
-import bitarray as ba
 import math
+import base64
 
-w = 16  # Размер слова в битах  (16, 32, 64)
-r = 20   # Кол-во раундов
-Key = "12312312312312"  # Секретный ключ
-print("KEY: ", Key)
-l = len(Key)  # Размер ключа в байтах
+w = int(input("Введите размер слова в битах  (16, 32, 64): "))  # Размер слова в битах  (16, 32, 64)
+r = int(input("Введите кол-во раундов: "))   # Кол-во раундов
+Key = input("Введите секретный ключ: ")  # Секретный ключ
 
-Key_bit = ba.bitarray()
-Key_bit.frombytes(Key.encode('utf-8'))
-while len(Key_bit) % w != 0:
-    Key_bit.insert(0, 0)
-l = int(len(Key_bit) / 8)
+Key_bit = base64.b64encode(bytes(Key, 'utf-8'))
+Key_bit = bytesToBin(Key_bit)  # Преобразование ключа в биты
 
-# K = list(K)
-#
-# for i in range(len(K)):  # Преобразование ключа в байты
-#     K[i] = ord(K[i])
-#
-# if (8 * l) % w != 0:  # Дополнение ключа до кратности с w
-#     K.extend([0] * int((w / 8) - l % (w / 8)))
-#     l = len(K)
+while len(Key_bit) % w != 0:  # Дополнение ключа до кратности w
+    Key_bit = "0" + Key_bit
+
+l = int(len(Key_bit) / 8)  # Размер ключа в байтах
 
 Pw = {16: 0xb7e1, 32: 0xb7e15163, 64: 0xb7e151628aed2a6b}
 Qw = {16: 0x9e37, 32: 0x9e3779b9, 64: 0x9e3779b97f4a7c15}
@@ -34,14 +25,7 @@ c = int(8 * l / w)  # число слов в ключе
 # Преобразование ключа в массив из с слов
 L = []
 for i in range(c):
-    L.append(int(Key_bit[i:i + w].to01(), 2))
-
-# L = []
-# for i in range(int(l / c)):
-#     temp = "0b"
-#     for j in range(i * c, i * c + c):
-#         temp += bin(K[j])[2:]
-#     L.append(int(temp, 2))
+    L.append(int("0b" + Key_bit[i:i + w], 2))
 
 for i in range(2 * r + 4 - 1):  # Инициализация массива раундовых ключей
     W.append(mod((W[-1] + Qw[w]), (2 ** w)))
@@ -49,36 +33,33 @@ for i in range(2 * r + 4 - 1):  # Инициализация массива ра
 i, j, a, b = 0, 0, 0, 0
 
 for count in range(3 * c):  # Формирование раундового ключа
-    # W[i] = shift(mod((W[i] + a + b), (2**w)), -3)
-    # W[i] = left_shift(mod((W[i] + a + b), (2 ** w)), 3, w)
     W[i] = circular_shift(mod((W[i] + a + b), (2 ** w)), w, 3, 'left')
     a = W[i]
-    # L[j] = shift(mod((L[j] + a + b), (2**w)), mod((a + b), (2**w)) % len(bin((L[j] + a + b))) - 2)
-    # L[j] = left_shift(mod((L[j] + a + b), (2 ** w)), mod(mod((a + b), (2 ** w)), len(bin((L[j] + a + b))) - 2), w)
     L[j] = circular_shift(mod((L[j] + a + b), (2 ** w)), w, mod((a + b), (2 ** w)), 'left')
     b = L[j]
     i = mod((i + 1), (2 * r + 4))
     j = mod((j + 1), c)
 
-# print(W)
+message = input("Введите сообщение: ")  # Сообщение
+print("MESSAGE: ", message)
+print("KEY: ", Key)
+
+message_bit = base64.b64encode(bytes(message, 'utf-8'))
+message_bit = bytesToBin(message_bit)  # Сообщение в битах
+
+while len(message_bit) % (4 * w) != 0:
+    message_bit = "0" + message_bit  # Дополнение нулями до кратности в 4w
+
+print("BIN MESSAGE: ", message_bit)
 
 # Шифрование
-message = "уууу  qqqq"  # Сообщение
-print("MESSAGE: ", message)
-message_bit = ba.bitarray()  # Инициализация массива битов
-message_bit.frombytes(message.encode('utf-8'))  # Сообщение в битах
-while len(message_bit) % (4 * w) != 0:
-    message_bit.insert(0, 0)  # Дополнение нулями до кратности в 4w
-print("BIN MESSAGE: ", message_bit.to01())
 encoded_message_bit = ""  # Инициализация зашифрованного сообщения
 
-# print('encode')
-
 for i in range(0, len(message_bit), 4 * w):  # Цикл по блокам в 4 слова
-    A = int(message_bit[i:i + w].to01(), 2)
-    B = int(message_bit[i + w:i + 2 * w].to01(), 2)
-    C = int(message_bit[i + 2 * w:i + 3 * w].to01(), 2)
-    D = int(message_bit[i + 3 * w:i + 4 * w].to01(), 2)
+    A = int('0b' + message_bit[i:i + w], 2)
+    B = int('0b' + message_bit[i + w:i + 2 * w], 2)
+    C = int('0b' + message_bit[i + 2 * w:i + 3 * w], 2)
+    D = int('0b' + message_bit[i + 3 * w:i + 4 * w], 2)
 
     B = mod(B + W[0], 2 ** w)
     D = mod(D + W[1], 2 ** w)
@@ -94,15 +75,15 @@ for i in range(0, len(message_bit), 4 * w):  # Цикл по блокам в 4 �
 
     A = mod(A + W[2 * r + 2], 2 ** w)
     C = mod(C + W[2 * r + 3], 2 ** w)
+
     encoded_message_bit += bin_expansion(bin(A), w)[2:] + bin_expansion(bin(B), w)[2:] + \
                            bin_expansion(bin(C), w)[2:] + bin_expansion(bin(D), w)[2:]
 
 print("ENCODED BIN MESSAGE: ", encoded_message_bit)
 
-# print('decode')
-
 # Дешифрование
-decoded_message_bit = ""
+decoded_message_bit = ""  # Инициализация расшифрованного сообщения
+
 for i in range(0, len(encoded_message_bit), 4 * w):
     A = int('0b' + encoded_message_bit[i:i + w], 2)
     B = int('0b' + encoded_message_bit[i + w:i + 2 * w], 2)
@@ -125,11 +106,10 @@ for i in range(0, len(encoded_message_bit), 4 * w):
 
     B = mod(B - W[0], 2 ** w)
     D = mod(D - W[1], 2 ** w)
+
     decoded_message_bit += bin_expansion(bin(A), w)[2:] + bin_expansion(bin(B), w)[2:] + \
                            bin_expansion(bin(C), w)[2:] + bin_expansion(bin(D), w)[2:]
 
 print("DECODED BIN MESSAGE: ", decoded_message_bit)
-import base64
-print("DECODED MESSAGE: ", base64.b16decode(hex(int(decoded_message_bit, base=2))[2:], casefold=True).decode('utf-8'))
-
+print("DECODED MESSAGE: ", base64.b64decode(binToBytes(decoded_message_bit)).decode('utf-8'))
 
